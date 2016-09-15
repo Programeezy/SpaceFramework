859897fb5a937c5e65e51f795aa89e4d23feaea9
Programezy <syavawow97@gmail.com>
2016-09-15 19:33:11 +0300

    ConstellationCollection

--- SpaceFramework/SpaceFramework/Constellation.cs
+++ SpaceFramework/SpaceFramework/Constellation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,4 +11,59 @@ namespace SpaceFramework
     {
 
     }
+
+    public class ConstellationCollection : ICollection<Constellation>
+    {
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(Constellation item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(Constellation item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Constellation[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Constellation> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Constellation item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
