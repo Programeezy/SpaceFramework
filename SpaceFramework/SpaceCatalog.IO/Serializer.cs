083e9f7bf70b2c4415164a18b8ac9328a0ffd07a
Programezy <syavawow97@gmail.com>
2016-11-12 20:00:55 +0300

    SpaceFileHandler + Serializer

---
+++ SpaceFramework/SpaceCatalog.IO/Serializer.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace SpaceCatalog.IO
+{
+    public static class SpaceSerializer 
+    {
+        private static Stream GetFile(string filename)
+        {
+            string dir_path = Path.GetDirectoryName(filename + ".xml");
+            Stream st = new FileStream(filename + ".xml", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            return st;
+        }
+
+        public static void Serialize<T>(T graph, string filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(graph.GetType(), filename);
+            using (TextWriter tw = new StreamWriter(GetFile(filename)))
+            {
+                serializer.Serialize(tw, graph);
+            }
+        }
+
+        public static T Deserialize<T>(string filename)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(T), filename);
+            using (TextReader tr = new StreamReader(GetFile(filename)))
+            {
+                try
+                {
+                    return (T)deserializer.Deserialize(tr);
+                }
+                catch(FileLoadException)
+                {
+                    throw new FileLoadException();
+                }
+            }
+        }
+       
+    }
+} 
---
+++ SpaceFramework/SpaceCatalog.IO/SpaceFileHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceCatalog;
+using System.IO;
+using System.IO.Compression;
+
+namespace SpaceCatalog.IO
+{
+    public static class SpaceFileHandler
+    {
+        static void WriteBinaryConstellation(string filename, Constellation constellation)
+        {
+
+        }
+
+        static void WriteBinaryStar(string filename, Star constellations)
+        {
+
+        }
+
+        static void WriteBinaryPlanet(string filename, Constellation constellations )
+        {
+            using (Stream fileSource = new FileStream(filename + ".bin", FileMode.Append))
+            {
+                using (DeflateStream ds = new DeflateStream(fileSource, CompressionMode.Compress))
+                {
+                    BinaryWriter bw = new BinaryWriter(ds);
+                    
+                }
+            }
+        }
+
+    }
+}
